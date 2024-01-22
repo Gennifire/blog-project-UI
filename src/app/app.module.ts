1abbc200990892c1e6346ce8263ff670be0db33a
Gennifire <Jenniferdee86@gmail.com>
2024-01-22 17:04:38 +0000

    core and category features - initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './Core/Components/nav-bar/nav-bar.component';
+import { CategoryListComponent } from './Features/Category/category-list/category-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    CategoryListComponent
   ],
   imports: [
     BrowserModule,
